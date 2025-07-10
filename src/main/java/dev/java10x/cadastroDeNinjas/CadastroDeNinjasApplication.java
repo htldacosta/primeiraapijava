9161925001ba344ccfd950b0ee87c11c021478ba
hitaludev <126851406+htldacosta@users.noreply.github.com>
2025-07-10 16:53:27 -0400

    Update CadastroDeNinjasApplication.java

--- src/main/java/dev/java10x/cadastroDeNinjas/CadastroDeNinjasApplication.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CadastroDeNinjasApplication {
-
+// teste
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroDeNinjasApplication.class, args);
-		System.out.println("hello");
+		System.out.println("hello world!!!");
 	}
 
 }
